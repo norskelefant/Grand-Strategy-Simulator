90a7906e4888e611e81f35949099505c35c08f77
Andreas Glückstad Nedergaard <norskelefant@gmail.com>
2026-07-06 17:10:19 +0200

    Ready to begin tackling construction tomorrow

---
+++ hoi_simulator/construction_types.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Constructions(Enum): 
+    CIV = "civ"
+    MIL = "mil"
+    DOCKYARD = "naval dockyard"
+    INFRASTRUCTURE = "infrastructure"
+    LAND_FORT = "land fort"
