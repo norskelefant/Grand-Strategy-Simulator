768ee81e8b0e793c31976ddb5e97c64b6e79555b
Andreas Glückstad Nedergaard <norskelefant@gmail.com>
2026-07-09 13:48:32 +0200

    Implemented hoi4 dates(who have no leap years)

--- hoi_simulator/date.py
+++ hoi_simulator/date.py
@@ -17,10 +17,10 @@ class Date:
         if self.month_has_28_days() == True and self.get_day() == 28: 
             self.month += 1
             self.day = 1
-        if self.month_has_30_days() == True and self.get_day() == 30: 
+        elif self.month_has_30_days() == True and self.get_day() == 30: 
             self.month += 1
             self.day = 1
-        if self.month_has_31_days() == True and self.get_day() == 31: 
+        elif self.month_has_31_days() == True and self.get_day() == 31: 
             if self.get_month() != 12: 
                 self.month += 1
                 self.day = 1
@@ -31,14 +31,49 @@ class Date:
         else: 
             self.day += 1
 
-    def month_has_31_days(self): 
-        return self.get_month() == 1 or self.get_month() == 3 or self.get_month() == 5 or self.get_month() == 7 or self.get_month() == 8 or self.get_month() == 10 or self.get_month() == 12
+    def next_month(self): 
+        if self.month_has_31_days(): 
+            for i in range(31 - self.get_day() + 1): 
+                self.increment_date()
+        elif self.month_has_30_days(): 
+            for i in range(30 - self.get_day() + 1): 
+                self.increment_date()
+        elif self.month_has_28_days(): 
+            for i in range(28 - self.get_day() + 1): 
+                self.increment_date()
+
+    def next_year(self): 
+        days_to_go_over = 0
+        #Counts only from 1 to before the current month
+        for i in range(1, self.get_month()): 
+            if self.month_has_31_days(i): 
+                days_to_go_over += 31
+            elif self.month_has_30_days(i): 
+                days_to_go_over += 30
+            elif self.month_has_28_days(i): 
+                days_to_go_over += 28
+        days_to_go_over += self.get_day()
+        for i in range(365 - days_to_go_over + 1): 
+            self.increment_date()
+
+    def month_has_31_days(self, month=None): 
+        if month == None: 
+            month = self.get_month()
+
+        return month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12
     
-    def month_has_30_days(self): 
-        return self.get_month() == 4 or self.get_month() == 6 or self.get_month() == 9 or self.get_month() == 11
+    def month_has_30_days(self, month=None): 
+        if month == None: 
+            month = self.get_month()
+
+        return month == 4 or month == 6 or month == 9 or month == 11
     
-    def month_has_28_days(self): 
-        return self.get_month() == 2
+    def month_has_28_days(self, month=None):
+        if month == None: 
+            month = self.get_month()
+
+        return month == 2
+    
     
     
 
--- testing/test_date.py
+++ testing/test_date.py
@@ -2,7 +2,7 @@ import hoi_simulator as hoi
 
 import pytest
 
-from hoi_simulator import country, state, setup_countries, construction, construction_types, construction_line, date
+from hoi_simulator import date
 
 @pytest.fixture
 def default_date(): 
@@ -20,26 +20,122 @@ def test_day_can_increase_by_1(default_date):
     assert default_date.get_month() == 1
     assert default_date.get_year() == 1936
 
-def test_month_can_increase_by_3(default_date): 
+def test_month_can_increase_by_1(default_date): 
     #Given default date
 
     #When the date increments
     for i in range(31): 
         default_date.increment_date()
 
-    #Then the day should be 2
+    #Then the mopnth should be 2
     assert default_date.get_day() == 1
     assert default_date.get_month() == 2
     assert default_date.get_year() == 1936
 
+def test_year_can_increase_by_1(default_date): 
+    #Given default date
 
+    #When the date increments
+    for i in range(365): 
+        default_date.increment_date()
 
+    #Then the month should be 2
+    assert default_date.get_day() == 1
+    assert default_date.get_month() == 1
+    assert default_date.get_year() == 1937
 
+def test_three_years_can_pass(default_date): 
+    #Given default date
+    
+    #When the year is increased by 3
+    for i in range(3): 
+        default_date.next_year()
+
+    #Then the year should be 1939
+    assert default_date.get_day() == 1
+    assert default_date.get_month() == 1
+    assert default_date.get_year() == 1939
+
+def test_year_can_pass_at_weird_date(default_date): 
+    #Given default date
+    
+    #When the date is made weird
+    for i in range(194): 
+        default_date.increment_date()
+    
+    assert default_date.get_day() == 14
+    assert default_date.get_month() == 7
+    assert default_date.get_year() == 1936
+
+    #And the next year is gone to
+    default_date.next_year()
+
+    #Then the year should be 1937
+    assert default_date.get_day() == 1
+    assert default_date.get_month() == 1
+    assert default_date.get_year() == 1937
+
+def test_year_can_pass_at_weird_date_two(default_date): 
+    #Given default date
+    
+    #When the date is made weird
+    for i in range(1000): 
+        default_date.increment_date()
+    
+    assert default_date.get_day() == 28
+    assert default_date.get_month() == 9
+    assert default_date.get_year() == 1938
+
+    #And the next year is gone to
+    default_date.next_year()
+
+    #Then the year should be 1939
+    assert default_date.get_day() == 1
+    assert default_date.get_month() == 1
+    assert default_date.get_year() == 1939
+
+def test_month_can_pass_at_weird_date(default_date): 
+    #Given default date
+    
+    #When the date is made weird
+    for i in range(3292): 
+        default_date.increment_date()
+    
+    assert default_date.get_day() == 8
+    assert default_date.get_month() == 1
+    assert default_date.get_year() == 1945
+
+    #And the next month is gone to
+    default_date.next_month()
+
+    #Then the month should be 2
+    assert default_date.get_day() == 1
+    assert default_date.get_month() == 2
+    assert default_date.get_year() == 1945
+
+def test_next_month_moves_year(default_date): 
+        #Given default date
+    
+    #When the date is made weird
+    for i in range(350): 
+        default_date.increment_date()
+    
+    assert default_date.get_day() == 17
+    assert default_date.get_month() == 12
+    assert default_date.get_year() == 1936
+
+    #And the next month is gone to
+    default_date.next_month()
+
+    #Then the month should be 1 and year 1937
+    assert default_date.get_day() == 1
+    assert default_date.get_month() == 1
+    assert default_date.get_year() == 1937
 
 def create_default_date(): 
     return date.Date(1, 1, 1936)
 
-
+#https://chatgpt.com/c/6a4f882a-5a18-83eb-b2ab-4a3eae557bbe
 
 
 
